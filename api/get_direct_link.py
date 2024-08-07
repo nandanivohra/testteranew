b8ee2da040828c585b41132ed2ef65553b4e5093
nandanivohra <147226303+nandanivohra@users.noreply.github.com>
2024-08-07 14:35:03 +0530

    Update get_direct_link.py

--- api/get_direct_link.py
+++ api/get_direct_link.py
@@ -185,7 +185,7 @@ class handler(BaseHTTPRequestHandler):
             return
 
         url = data.get('url')
-        ndus = 'YTkpr3MteHuiPVsBs_GR5xSp4m6_Lq9C-JDxZK2h'  # Replace with your actual ndus value
+        ndus = 'YyUxbCKteHuiwuuBZv-Xkohjr_-SR8OhjbcbEW2e'  # Replace with your actual ndus value
 
         if not url:
             self.send_error(400, "Missing URL parameter")
